89468631ce94ee96cb634710d50dae8826a2153d
sebastienCook <43784198+sebastienCook@users.noreply.github.com>
2019-03-11 14:31:04 -0400

    Update FaultTimer.java

--- src/FaultTimer.java
+++ src/FaultTimer.java
@@ -1,91 +1,66 @@
-//@Author Andrew Dybka, Maveric
-//Create timers to make sure elevators are runn
-
-
-
 import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
-
-
-public class FaultTimer {
-	private Scheduler thisSched;
-	private int elevatorNumber;
-	private Timer timer;
-	private boolean running;
-
-	public FaultTimer(Scheduler s, int eNum) {
-		thisSched = s;
-		elevatorNumber = eNum;
-		running = false;
+public class FaultScheduler extends Thread{
+	private Scheduler s;
+	private int preState, preFloor, elevator;
+	public FaultScheduler(Scheduler s, int elev) {
+		this.s = s;
+		elevator = elev;
 	}
+	public void run() {
+		while(true) {
+			System.out.println("FaultScheduler for elevator " + elevator + " Checking at: " + new Date());
+			try {
+				if(elevator == 1) {
+					preState = s.elevatorState1;
+					preFloor = s.elevatorFloor1;
+				}
+				else if(elevator == 2) {
+					preState = s.elevatorState2;
+					preFloor = s.elevatorFloor2;
+				}
+				else {
+					preState = s.elevatorState3;
+					preFloor = s.elevatorFloor3;
+				}
+				Thread.sleep(5000);
 
-	private void startTimer() {//create a new timer
-		TimerTask timerTask = new TimerTask() {
-			@Override
-			public void run() {
-				if (elevatorNumber == 1) {
-					thisSched.e1Function = false;
-					System.out.println("^^^^Elevator 1 shutting down^^^^");
+				if(elevator == 1) {
+					System.out.println("ELEVATOR 1: preFloor: " + preFloor + ", State: " + s.elevatorState1 + " current Floor: " + s.elevatorFloor1);
+					if(s.elevatorState1 != 0 &&  preFloor == s.elevatorFloor1) {
+						System.out.println("smt broke");
+						s.elevatorState1 = 3;
+						s.isActive1 = false;
+						System.out.println("^^^ERROR: ELEVATOR 1 BROKEN^^^^");
+					}
 				}
-				if (elevatorNumber == 2) {
-					thisSched.e2Function = false;
-					System.out.println("^^^^Elevator 2 shutting down^^^^");
+				else if(elevator == 2) {
+					System.out.println("ELEVATOR2: preFloor: " + preFloor + ", State: " + s.elevatorState2 + " current Floor: " + s.elevatorFloor2);
+					if(s.elevatorState2 != 0 &&  preFloor == s.elevatorFloor2) {
+						System.out.println("smt broke");
+						s.elevatorState2 = 3;
+						s.isActive2 = false;
+						System.out.println("^^^ERROR: ELEVATOR 2 BROKEN^^^^");
+
+					}
 				}
-				if (elevatorNumber == 3) {
-					thisSched.e3Function = false;
-					System.out.println("^^^^Elevator 3 shutting down^^^^");
+				else if (elevator == 3){ //elevator 3
+					System.out.println("ELEVATOR 3: preFloor: " + preFloor + ", State: " + s.elevatorState3 + " current Floor: " + s.elevatorFloor3);
+					if(s.elevatorState3 != 0 && preFloor == s.elevatorFloor3) {
+						System.out.println("smt broke");
+						s.elevatorState3 = 3;
+						s.isActive3 = false;
+						System.out.println("^^^ERROR: ELEVATOR 3 BROKEN^^^^");
+
+					}
+					
 				}
+				else {
+					System.out.println("Elevator " + elevator + " appears to be working as expected");
+				}
+				//So elevator is attached @ creation... checks elevator state which better update if it returns as fault 
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-		};
-		//
-		//create a new timer and set running to true
-		
-		timer = new Timer();
-		timer.schedule(timerTask, 3000);
-		running = true;
-
-	}
-
-	private void update() {//update a started timer
-		TimerTask timerTask = new TimerTask() {
-			@Override
-			public void run() {
-				if (elevatorNumber == 1) {
-					thisSched.e1Function = false;
-					System.out.println("^^^^Elevator 1 shutting down^^^^");
-				}
-				if (elevatorNumber == 2) {
-					thisSched.e2Function = false;
-					System.out.println("^^^^Elevator 2 shutting down^^^^");
-				}
-				if (elevatorNumber == 3) {
-					thisSched.e3Function = false;
-					System.out.println("^^^^Elevator 3 shutting down^^^^");
-				}
-			}
-		};
-		//clear old timer and create new with task
-		
-		timer.cancel();
-		timer = new Timer();
-		timer.schedule(timerTask, 3000);
-	}
-
-	public void stopTimer() {
-		if(running) {
-			timer.cancel();
-			running = false;
-		}
-
-	}
-
-	public void time() {
-		if (running) {//if already runnign update timer
-			update();
-		} else {//if not timeing start a new timer
-			startTimer();
 		}
 	}
-
 }
