eb6a92b32d71cdee3af7cecdd34cb916ae2fd62e
sebastienCook <43784198+sebastienCook@users.noreply.github.com>
2019-04-02 13:42:22 -0400

    Update Floor.java

--- src/Floor.java
+++ src/Floor.java
@@ -6,6 +6,7 @@ import java.net.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -14,95 +15,58 @@ public class Floor {
 	DatagramSocket sendReceiveSocket;
 	
 	static String fileName = ".//input.txt";
-	static int numOfFloors = 10;
+	
 	static int SCHEDULER_PORT = 219, SELFPORT = 238;
 	static String temp;
 	static List<String> allLines;       // input file content
 	
-	Floor Floors[];
+	Floor_Gui gui;
 	
-	int currentFloor;            // the floor of the current object. 2 is lobby
+	
 	int elevatorDirection; 		// 0 is stop, 1 up, 2 down
-	int floorButton;      		// 1 is going up, 0 is going down
-	int floorLamp;              // 1 is waiting, 0 is not waiting
-	int directionLamp;          // 2 is for off, 1 is for going up, 0 is going down
+	int numOfFloors;
+	ArrayList<Integer> request;
 	
 	// Constructor with custom floor level
-	public Floor(int currFloor, int port) {
+	public Floor(int floor, int port) {
+		request = new ArrayList<Integer>();
+		numOfFloors = floor;
+		gui = new Floor_Gui(floor,this);
+		
 	   try {
 	      // Construct a datagram socket and bind it to any available port on the local host machine
 	      sendReceiveSocket = new DatagramSocket(port);
-	      if (currFloor > numOfFloors) {
-	    	  System.out.println("This floor is too high");
-	    	  System.exit(1);
-	      }
-	      this.currentFloor = currFloor;
-	      this.floorLamp = 0;
-	      this.directionLamp = 2;
-	      System.out.println("Floor " + currentFloor + " created.");
+	      
 	   } catch (SocketException se) {   // Can't create the socket.
 	      se.printStackTrace();
 	      System.exit(1);
 	   }
+	   
+	   this.receiveMessage();
 	}
 	
 	
-	public void sendInstructions(String line) throws InterruptedException {
-	// For testing, prints line of the input file
-		System.out.println("Request made: " + line);	
-
-		 String[] splitted = line.split("\\s+");
-
-		 byte msg[] = new byte[5];	// Bit 0 - Direction 	Bit 1,2 - initial Floor   Bit 3,4 - des Floor
-	
-		 // Direction
-		 if (splitted[2].equals("up")) {
-		   msg[0] = 1;
-		   this.directionLamp = 1;
-		   this.floorButton = 1;
-		 } else if (splitted[2].equals("down")) {
-		   msg[0] = 0;
-		   this.directionLamp = 0;
-		   this.floorButton = 0;
-		 } else {
-		   System.out.println("Input file format incorrect");
-		   System.exit(1);
+	public void sendInstructions(int floor, int direction) {
+		
+		 this.request.add(floor);
+		 byte msg[] = new byte[5];	// Bit 0 - Direction 	Bit 1,2 - destination Floor   
+		 msg[0] = (byte)direction;
+		 if(floor>19) {
+			 msg[1]=2;
+			 msg[2] = (byte)(floor-20);
 		 }
-
-		 // Current floor
-		 if (splitted[1].length() > 1) {	// if floor is in double digits
-		     byte n1 = (byte) (Integer.parseInt(splitted[1])/10);	// get first digit of string
-		     byte n2 = (byte) (Integer.parseInt(splitted[1])%10);	// get last digit of string
-		     msg[1] = n1;
-		     msg[2] = n2;
-		 } else {
-		     byte n = (byte) (Integer.parseInt(splitted[1]));
-		     msg[1] = 0;
-		     msg[2] = n;
-		 }
-
-		 
-		 if(splitted[3].length()>1) {//destination floor is double digits
-		     byte d1 = (byte) (Integer.parseInt(splitted[3])/10);	// get first digit of string
-		     byte d2 = (byte) (Integer.parseInt(splitted[3])%10);	// get last digit of string
-		     msg[3] = d1;
-		     msg[4] = d2; 
+		 else if(floor>9) {
+			 msg[1]=1;
+			 msg[2] = (byte)(floor-10);
 		 }
 		 else {
-			 byte d = (byte) (Integer.parseInt(splitted[3]));
-			 msg[3] = 0;
-			 msg[4] = d;
+			 msg[1]=0;
+			 msg[2] = (byte)floor;
 		 }
-
+		msg[3] =0;
+		msg[4] =0;
 		 try {
-		    sendPacket = new DatagramPacket(msg, msg.length,
-				    InetAddress.getLocalHost(), SCHEDULER_PORT);
-		 } catch (UnknownHostException e) {
-		    e.printStackTrace();
-		    System.exit(1);
-		 }
-
-		 try {
+		    sendPacket = new DatagramPacket(msg, msg.length,InetAddress.getLocalHost(), SCHEDULER_PORT);
 		    sendReceiveSocket.send(sendPacket);
 		 } catch (IOException e) {
 		    e.printStackTrace();
@@ -115,21 +79,23 @@ public class Floor {
 		 }
 		 System.out.println("\nElevator request sent.\n");
 		 
-		 
-		 try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			System.out.println("cant Sleep");
-		}
 	}
 
+	public void verify(int floor) {
+		int count=0;
+		for(int fl: this.request) {
+			if(floor==fl) {
+				this.request.remove(count);
+			}
+			count++;
+		}
+	}
 	
 	// For iteration 5
 	public void receiveMessage() {
 		// Now receiving
-		 byte data[] = new byte[3];
-		 receivePacket = new DatagramPacket(data, data.length);
+		 byte temp[] = new byte[3];
+		 receivePacket = new DatagramPacket(temp, temp.length);
 		 try {
 		    // Block until a datagram is received via sendReceiveSocket.  
 		    sendReceiveSocket.receive(receivePacket);
@@ -137,61 +103,26 @@ public class Floor {
 		    e.printStackTrace();
 		    System.exit(1);
 		 }
+		 int eleFloor;
+		 	if(temp[2]>=20) {
+		 		eleFloor = temp[3] + 20;
+			}
+			else if (temp[2] >= 10) {
+				eleFloor = temp[3] + 10;
+			} else {
+				eleFloor = temp[3];
+			}
+	//	this.verify(eleFloor);///////////////////////////////////////////////////
+		 
 		 System.out.print("Received content containing: ");
 		 // Form a String from the byte array.
-		 String received = new String(data,0,receivePacket.getLength());   
+		 String received = new String(temp,0,receivePacket.getLength());   
 		 System.out.println(received);
 	}
 	
 	public static void main(String args[])
 	{
-	   Floor Floors[] = new Floor[numOfFloors]; 
-	   for (int i = 1; i <= numOfFloors; i++) {
-		  Floors[i-1] = new Floor(i, SELFPORT + i);
-	   }
-		  
-	// Reads input files for elevator calls
-	   Path path = Paths.get(fileName); 
-	   try {
-	 	  allLines = Files.readAllLines(path);
-	 	  int firstFloor = 0;
-	 	  int lastTime = 0;
-	 	  for (String line : allLines) {
-				temp = line;
-				String[] splitted = line.split("\\s+");		// Splits line
-				int floorNum = Integer.parseInt(splitted[1]);	// Get the floor number from line
-				
-				// if its not the first floor thats read from input.txt, we subtract the lastTime from the current time to find out how long to wait
-				if (firstFloor != 0) {
-					 // split splitted[0] by : to get ms, sec, etc
-					 String[] timeDelay = splitted[0].split(":");
-					 int msToWait = 0;
-					 msToWait += Integer.parseInt(timeDelay[3]);	// deals with ms
-					 msToWait += (1000*Integer.parseInt(timeDelay[2]));		// deals with s
-					 msToWait += (1000*60*Integer.parseInt(timeDelay[1]));		// deals with minutes
-					 msToWait += (1000*60*60*Integer.parseInt(timeDelay[0])); 	// deals with hours
-					 System.out.println("Time to wait: " + (msToWait - lastTime)/1000 + " seconds");
-					 Thread.sleep(msToWait - lastTime);
-					 lastTime = msToWait;
-				} else {
-					// split splitted[0] by : to get ms, sec, etc
-					String[] timeDelay = splitted[0].split(":");
-					lastTime += Integer.parseInt(timeDelay[3]);
-					lastTime += (1000*Integer.parseInt(timeDelay[2]));
-					lastTime += (1000*60*Integer.parseInt(timeDelay[1]));
-					lastTime += (1000*60*60*Integer.parseInt(timeDelay[0]));
-					System.out.println("Time to wait: 0 seconds");
-					firstFloor = 1;
-				}
-				
-				Floors[floorNum].sendInstructions(temp);
-				
-	     }
-	   } catch (IOException e) {
-	  	e.printStackTrace();  
-	   } catch (InterruptedException e) {
-		e.printStackTrace();
-	}
+	   Floor Floors = new Floor(22,SELFPORT);
 	   
 	}
 }
