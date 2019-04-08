0a0d43f75b15b8a74dba0e959d796fea4bf8d1e9
Sean Wallach <wallachsean1@gmail.com>
2019-04-08 13:15:20 -0400

    Add files via upload

---
+++ TestClass.java
@@ -0,0 +1,16 @@
+
+// This test class runs all parts of 
+public class TestClass {
+	public void main(String[] string) {
+		// Testing elevator
+		Elevator elevator = new Elevator();
+		// Should create 4 elevs
+		
+		
+		// Testing scheduler
+		Scheduler s = new Scheduler();
+		
+		// Testing floor 
+		Floor floor = new Floor(1,11);		
+	}
+}
